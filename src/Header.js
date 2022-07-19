5a783b4ef932980b008e6eb45bfb7563b1b8b9f9
mhaspis <mhaspis@gmail.com>
2022-07-19 13:55:36 -0300

    NavBar

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+function Header(){
+       return (
+    <header>
+        <h1>Titulo</h1>
+        <nav>
+            <a href="#">Suspenso </a>
+            <a href="#">Terror </a>
+            <a href="#">Novela </a>
+        </nav>
+    </header>
+       ) 
+        
+
+}
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+function NavBar(){
+    return (
+        <header>
+            <h1 className="titulo">Libreria Mhaspis</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home </a></li>
+                    <li><a href="#">Ciencia Ficcion </a></li> 
+                    <li><a href="#">Suspenso </a></li>
+                    <li><a href="#">Terror </a></li>
+                    <li><a href="#">Novela Romanticas</a></li>
+                    <li><a href="#">Fantasia </a></li> 
+                    <li><a href="#">Politica </a></li> 
+                </ul>                   
+            </nav>
+        </header>
+    ) 
+     
+
+}
+
+export default NavBar
